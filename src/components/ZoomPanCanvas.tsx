06a008d296083ce989cc537875c9f2dd2a8cf9cb
Oli Legat <olivier.legat@ag-grid.com>
2025-10-23 17:15:12 +0100

    fix zoompan (on x-axis)

--- src/components/ZoomPanCanvas.tsx
+++ src/components/ZoomPanCanvas.tsx
@@ -31,10 +31,16 @@ interface TouchOrigin {
 
 const N = 1_000_000;
 
-// Interpolate `a` from [Rx, Rw] to [min, max]
+// Interpolate `a` from client [Rx, Rw] to normal [min, max]
 function toNormal({ min, max }: MinMax, a: number, Rx: number, Rw: number): number {
     if (Rw === 0) return 0; // don't divide by 0.
     return N * (((a - Rx) / Rw) * (max - min) + min);
+}
+
+// Interpolate `x` from normal [min, max] to client [Rx, Rw]
+function toClient({ min, max }: MinMax, x: number, Rx: number, Rw: number): number {
+  if (max === min) return Rx; // avoid div by 0
+  return Rx + (Rw * (x / N - min)) / (max - min);
 }
 
 // Solve the normalized min/max state for two give touch points
@@ -55,22 +61,22 @@ function solveTwoUnknowns(x1: number, x2: number, a1: number, a2: number, Rx: nu
 
 export default function ZoomPanCanvas({ src, width = 400, height = 300 }: ZoomPanCanvasProps) {
   const containerRef = useRef<HTMLDivElement | null>(null);
-  const [transform, setTransform] = useState<Transform>({ translationX: 0, translationY: 0, scaleX: 1, scaleY: 1 });
+  const [transform, setTransform] = useState<Transform>({ translationX: 0, translationY: 0, scaleX: 2, scaleY: 1 });
 
   const gestureRef = useRef({
-    initialZoom: { x: { min: 0, max: 1 }, y: { min: 0, max: 1 } },
-    currentZoom: { x: { min: 0, max: 1 }, y: { min: 0, max: 1 } },
+    initialZoom: { x: { min: 0, max: N/2 }, y: { min: 0, max: N } },
+    currentZoom: { x: { min: 0, max: N/2 }, y: { min: 0, max: N } },
     origins: [
       { identifier: NaN, normalX: NaN, normalY: NaN },
       { identifier: NaN, normalX: NaN, normalY: NaN },
     ] as [TouchOrigin, TouchOrigin], // TODO(olegat) remove `as`
   });
 
-  function updateTransform(zoom: NormalZoom, Ry: number, Rw: number, Rh: number) {
-    const scaleX = Rw / (zoom.x.max - zoom.x.min);
-    const scaleY = Rh / (zoom.y.max - zoom.y.min);
-    const translationX = -zoom.x.min * scaleX;
-    const translationY = -(Ry + Rh - zoom.y.min * scaleY);
+  function updateTransform(zoom: NormalZoom, Rx: number, Ry: number, Rw: number, Rh: number) {
+    const scaleX = N / (zoom.x.max - zoom.x.min);
+    const scaleY = N / (zoom.y.max - zoom.y.min);
+    const translationX = toClient(zoom.x, zoom.x.min, Rx, Rw);
+    const translationY = 0//toClient(zoom.y, zoom.y.min, Ry, Rh);
     setTransform({ translationX, translationY, scaleX, scaleY });
   }
 
@@ -78,6 +84,7 @@ export default function ZoomPanCanvas({ src, width = 400, height = 300 }: ZoomPa
     if (e.targetTouches.length === 2 && containerRef.current != null) {
       e.preventDefault();
       const { origins, initialZoom } = gestureRef.current;
+      console.log(initialZoom);
       const { x: Rx, y: Ry, width: Rw, height: Rh } = containerRef.current.getBoundingClientRect();
       for (const i of [0, 1]) {
         const a = e.targetTouches[i].clientX;
@@ -114,8 +121,8 @@ export default function ZoomPanCanvas({ src, width = 400, height = 300 }: ZoomPa
       const b2 = Ry + Rh - touches[1].clientY;
 
       currentZoom.x = solveTwoUnknowns(x1, x2, a1, a2, Rx, Rw);
-      currentZoom.y = solveTwoUnknowns(y1, y2, b1, b2, Rx, Rw);
-      updateTransform(currentZoom, Ry, Rw, Rh);
+      //currentZoom.y = solveTwoUnknowns(y1, y2, b1, b2, Ry, Rh);
+      updateTransform(currentZoom, Rx, Ry, Rw, Rh);
     }
   };
 
@@ -143,7 +150,7 @@ export default function ZoomPanCanvas({ src, width = 400, height = 300 }: ZoomPa
             translate(${transform.translationX}px, ${transform.translationY}px)
             scale(${transform.scaleX}, ${transform.scaleY})
           `,
-          transformOrigin: "center center",
+          transformOrigin: "left top",
         }}
       />
     </div>
