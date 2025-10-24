9aa1c917370540bd637e0b0108324a156b9e1ca6
Oli Legat <olivier.legat@ag-grid.com>
2025-10-24 10:25:01 +0100

    decouple the math & React logic

--- src/components/ZoomPan.tsx
+++ src/components/ZoomPan.tsx
@@ -1,4 +1,5 @@
-import { useRef, useState } from "react";
+import { useRef, useState } from 'react';
+import { ZoomPanSolver } from './ZoomPanSolver';
 
 interface ZoomPanProps {
   src: string;
@@ -6,127 +7,38 @@ interface ZoomPanProps {
   height?: number | `${number}%`;
 }
 
-interface Transform {
-  translationX: number;
-  translationY: number;
-  scaleX: number;
-  scaleY: number;
-}
-
-interface MinMax {
-  min: number;
-  max: number;
-}
-
-interface NormalZoom {
-  x: MinMax;
-  y: MinMax;
-}
-
-interface TouchOrigin {
-  identifier: number;
-  normalX: number;
-  normalY: number;
-}
-
-const N = 1_000_000;
-
-// Interpolate `a` from client [Rx, Rw] to normal [min, max]
-function toNormal({ min, max }: MinMax, a: number, Rx: number, Rw: number): number {
-    if (Rw === 0) return 0; // don't divide by 0.
-    return N * (((a - Rx) / Rw) * (max - min) + min);
-}
-
-// Interpolate `x` from normal [min, max] to client [Rx, Rw]
-function toClient({ min, max }: MinMax, x: number, Rx: number, Rw: number): number {
-  if (max === min) return Rx; // avoid div by 0
-  return Rx + (Rw * (x / N - min)) / (max - min);
-}
-
-// Solve the normalized min/max state for two give touch points
-function solveTwoUnknowns(x1: number, x2: number, a1: number, a2: number, Rx: number, Rw: number): MinMax {
-    // The math expects x1 <= x2 (and a1 <= a2).
-    // If x1 > x2, then the gesture will be reversed (i.e. fingers moving closer would zoom in).
-    [x1, x2] = [Math.min(x1, x2), Math.max(x1, x2)];
-    [a1, a2] = [Math.min(a1, a2), Math.max(a1, a2)];
-
-    const t1 = (N * (a1 - Rx)) / Rw;
-    const t2 = (N * (a2 - Rx)) / Rw;
-    const c = (a1 - Rx) / (a2 - Rx); // === (t1 / t2);
-
-    const min = (x1 - c * x2) / (N - t1 + c * (t2 - N));
-    const max = (x2 + (t2 - N) * min) / t2;
-    return { min, max };
-}
-
 export default function ZoomPan({ src, width = 400, height = 300 }: ZoomPanProps) {
   const containerRef = useRef<HTMLDivElement | null>(null);
-  const [transform, setTransform] = useState<Transform>({ translationX: 0, translationY: 0, scaleX: 1, scaleY: 1 });
+  const { current: solver } = useRef<ZoomPanSolver>(new ZoomPanSolver());
 
-  const gestureRef = useRef({
-    initialZoom: { x: { min: 0, max: N }, y: { min: 0, max: N } },
-    currentZoom: { x: { min: 0, max: N }, y: { min: 0, max: N } },
-    origins: [
-      { identifier: NaN, normalX: NaN, normalY: NaN },
-      { identifier: NaN, normalX: NaN, normalY: NaN },
-    ] as [TouchOrigin, TouchOrigin], // TODO(olegat) remove `as`
-  });
-
-  function updateTransform(zoom: NormalZoom, Rx: number, Ry: number, Rw: number, Rh: number) {
-    const scaleX = N / (zoom.x.max - zoom.x.min);
-    const scaleY = N / (zoom.y.max - zoom.y.min);
-    const translationX = toClient(zoom.x, zoom.x.min, Rx, Rw);
-    const translationY = Rh - toClient(zoom.y, zoom.y.min, Ry, Rh);
-    setTransform({ translationX, translationY, scaleX, scaleY });
-  }
+  const [transform, setTransform] = useState(`translate(0px, 0px) scale(1, 1)`);
 
   const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
     if (e.targetTouches.length === 2 && containerRef.current != null) {
       e.preventDefault();
-      const { origins, initialZoom } = gestureRef.current;
-      const { x: Rx, y: Ry, width: Rw, height: Rh } = containerRef.current.getBoundingClientRect();
-      for (const i of [0, 1]) {
-        const a = e.targetTouches[i].clientX;
-        const b = Ry + Rh - e.targetTouches[i].clientY;
-        origins[i].identifier = e.targetTouches[i].identifier;
-        origins[i].normalX = toNormal(initialZoom.x, a, Rx, Rw);
-        origins[i].normalY = toNormal(initialZoom.y, b, Ry, Rh);
-      }
+
+      // Initialise the math:
+      const rect = containerRef.current.getBoundingClientRect();
+      solver.handleTouchStart(rect, e.targetTouches[0], e.targetTouches[1]);
     }
   };
 
   const handleTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
-    const { origins, currentZoom } = gestureRef.current;
     if (containerRef.current != null && e.targetTouches.length === 2) {
       e.preventDefault();
-      const { x: Rx, y: Ry, width: Rw, height: Rh } = containerRef.current.getBoundingClientRect();
 
-      // The ordering of the elements in TouchEvent.targetTouches is not guaranteed to be the same in 'touchstart'
-      // and 'touchend', so cannot assume that the indices of `origins` and `targetTouches` line-up. The
-      // `.map().find()` operations guarantees a result such that `touch[i].identifier === origins[i].identifier`.
-      //
-      // We can use the `!` operator here to remove the `undefined` type from the return-type of `.find()` because
-      // we can assume that all the identifies in `origins` (set at 'touchstart') can be found in `targetTouches`
-      // (set at 'touchmove').
-      const targetTouches = Array.from(e.targetTouches);
-      const touches = [0, 1].map((i) => targetTouches.find((t) => t.identifier === origins[i].identifier)!);
-      const x1 = origins[0].normalX;
-      const x2 = origins[1].normalX;
-      const a1 = touches[0].clientX;
-      const a2 = touches[1].clientX;
-      const y1 = origins[0].normalY;
-      const y2 = origins[1].normalY;
-      const b1 = Ry + Rh - touches[0].clientY;
-      const b2 = Ry + Rh - touches[1].clientY;
+      // Solve the math
+      const rect = containerRef.current.getBoundingClientRect();
+      solver.handleTouchMove(rect, e.targetTouches[0], e.targetTouches[1]);
 
-      currentZoom.x = solveTwoUnknowns(x1, x2, a1, a2, Rx, Rw);
-      currentZoom.y = solveTwoUnknowns(y1, y2, b1, b2, Ry, Rh);
-      updateTransform(currentZoom, Rx, Ry, Rw, Rh);
+      // Update React state
+      const { scaleX, scaleY, translationX, translationY } = solver.toCSSTransform(rect)
+      setTransform(`translate(${translationX}px, ${translationY}px) scale(${scaleX}, ${scaleY})`);
     }
   };
 
   const handleTouchEnd = (_e: React.TouchEvent<HTMLDivElement>) => {
-    gestureRef.current.initialZoom = gestureRef.current.currentZoom;
+    solver.handleTouchEnd();
   };
 
   return (
@@ -141,16 +53,7 @@ export default function ZoomPan({ src, width = 400, height = 300 }: ZoomPanProps
       <img
         src={src}
         alt="Zoomable"
-        style={{
-          width: "100%",
-          height: "100%",
-          objectFit: "cover",
-          transform: `
-            translate(${transform.translationX}px, ${transform.translationY}px)
-            scale(${transform.scaleX}, ${transform.scaleY})
-          `,
-          transformOrigin: "left top",
-        }}
+        style={{ width: "100%", height: "100%", objectFit: "cover", transform, transformOrigin: "left top" }}
       />
     </div>
   );
---
+++ src/components/ZoomPanSolver.ts
@@ -0,0 +1,125 @@
+interface MinMax {
+  min: number;
+  max: number;
+}
+
+interface ClientBounds {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
+interface OriginTouch {
+  identifier: number;
+  normalX: number;
+  normalY: number;
+}
+
+interface TargetTouch {
+  identifier: number;
+  clientX: number;
+  clientY: number;
+}
+
+interface Transform {
+  translationX: number;
+  translationY: number;
+  scaleX: number;
+  scaleY: number;
+}
+
+const N = 1_000_000;
+
+// Interpolate `a` from client [Rx, Rw] to normal [min, max]
+function toNormal({ min, max }: MinMax, a: number, Rx: number, Rw: number): number {
+    if (Rw === 0) return 0; // don't divide by 0.
+    return N * (((a - Rx) / Rw) * (max - min) + min);
+}
+
+// Interpolate `x` from normal [min, max] to client [Rx, Rw]
+function toClient({ min, max }: MinMax, x: number, Rx: number, Rw: number): number {
+  if (max === min) return Rx; // avoid div by 0
+  return Rx + (Rw * (x / N - min)) / (max - min);
+}
+
+// Solve the normalized min/max state for two give touch points
+function solveTwoUnknowns(x1: number, x2: number, a1: number, a2: number, Rx: number, Rw: number): MinMax {
+  // The math expects x1 <= x2 (and a1 <= a2).
+  // If x1 > x2, then the gesture will be reversed (i.e. fingers moving closer would zoom in).
+  [x1, x2] = [Math.min(x1, x2), Math.max(x1, x2)];
+  [a1, a2] = [Math.min(a1, a2), Math.max(a1, a2)];
+
+  const t1 = (N * (a1 - Rx)) / Rw;
+  const t2 = (N * (a2 - Rx)) / Rw;
+  const c = (a1 - Rx) / (a2 - Rx); // === (t1 / t2);
+
+  const min = (x1 - c * x2) / (N - t1 + c * (t2 - N));
+  const max = (x2 + (t2 - N) * min) / t2;
+  return { min, max };
+}
+
+export class ZoomPanSolver {
+  private initialZoomX: MinMax = { min: 0, max: N };
+  private initialZoomY: MinMax = { min: 0, max: N };
+  private currentZoomX: MinMax = { min: 0, max: N };
+  private currentZoomY: MinMax = { min: 0, max: N };
+  private readonly origins1: OriginTouch = { identifier: NaN, normalX: NaN, normalY: NaN };
+  private readonly origins2: OriginTouch = { identifier: NaN, normalX: NaN, normalY: NaN };
+
+  handleTouchStart(clientRect: ClientBounds, targetTouch1: TargetTouch, targetTouch2: TargetTouch): void {
+    const { x: Rx, y: Ry, width: Rw, height: Rh } = clientRect;
+    let a: number, b: number;
+
+    a = targetTouch1.clientX;
+    b = Ry + Rh - targetTouch1.clientY;
+    this.origins1.identifier = targetTouch1.identifier;
+    this.origins1.normalX = toNormal(this.initialZoomX, a, Rx, Rw);
+    this.origins1.normalY = toNormal(this.initialZoomY, b, Ry, Rh);
+
+    a = targetTouch2.clientX;
+    b = Ry + Rh - targetTouch2.clientY;
+    this.origins2.identifier = targetTouch2.identifier;
+    this.origins2.normalX = toNormal(this.initialZoomX, a, Rx, Rw);
+    this.origins2.normalY = toNormal(this.initialZoomY, b, Ry, Rh);
+  }
+
+  handleTouchMove(clientRect: ClientBounds, targetTouch1: TargetTouch, targetTouch2: TargetTouch): void {
+    const { x: Rx, y: Ry, width: Rw, height: Rh } = clientRect;
+    const { origins1, origins2 } = this;
+
+    // The ordering of the elements in TouchEvent.targetTouches is not guaranteed to be the same in 'touchstart'
+    // and 'touchend', so cannot assume that the indices of `origins` and `targetTouches` line-up.
+    const touches1 = origins1.identifier === targetTouch1.identifier ? targetTouch1 : targetTouch2;
+    const touches2 = origins2.identifier === targetTouch2.identifier ? targetTouch2 : targetTouch1;
+
+    const x1 = origins1.normalX;
+    const x2 = origins2.normalX;
+    const a1 = touches1.clientX;
+    const a2 = touches2.clientX;
+    const y1 = origins1.normalY;
+    const y2 = origins2.normalY;
+    const b1 = Ry + Rh - touches1.clientY;
+    const b2 = Ry + Rh - touches2.clientY;
+
+    this.currentZoomX = solveTwoUnknowns(x1, x2, a1, a2, Rx, Rw);
+    this.currentZoomY = solveTwoUnknowns(y1, y2, b1, b2, Ry, Rh);
+  }
+
+  handleTouchEnd(): void {
+    this.initialZoomX = this.currentZoomX;
+    this.initialZoomY = this.currentZoomY;
+  }
+
+  toCSSTransform(clientRect: ClientBounds): Transform {
+    const { x: Rx, y: Ry, width: Rw, height: Rh } = clientRect;
+    const { currentZoomX: zoomX, currentZoomY: zoomY } = this;
+
+    return {
+      scaleX: N / (zoomX.max - zoomX.min),
+      scaleY: N / (zoomY.max - zoomY.min),
+      translationX: toClient(zoomX, zoomX.min, Rx, Rw),
+      translationY: Rh - toClient(zoomY, zoomY.min, Ry, Rh),
+    };
+  }
+}
