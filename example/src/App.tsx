03b77f28c0d1ed322849f66227b80e316120a655
Oli Legat <olivier.legat@ag-grid.com>
2025-11-13 16:13:24 +0000

    add sheep.jpeg

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,7 +3,7 @@ import ZoomPan from "./components/ZoomPan";
 export default function App() {
   return (
     <div style={{  textAlign: "center", marginTop: "2rem" }}>
-      <ZoomPan width="100%" height="100%" src="https://www.w3schools.com/html/pic_trulli.jpg" />
+      <ZoomPan width="100%" height="100%" src="./sheep.jpeg" />
     </div>
   );
 }
